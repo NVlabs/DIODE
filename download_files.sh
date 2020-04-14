d0d91f10d416dce95dbc356846b63d1d93344f18
Akshay Chawla <achawla@nvidia.com>
2020-04-14 13:28:53 -0700

    Download necessary files + runner script
    1. Download necessary files + checkpoints using $bash download_files.sh
    2. run the code using runner_yolo.sh which will save generated images in ./yoloResults folder

---
+++ download_files.sh
@@ -0,0 +1,15 @@
+# ./serialized_dict.pkl
+# https://drive.google.com/file/d/1RvgZMELiIdm_peFKdJiHbIwVtgu55X6z/view?usp=sharing
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1RvgZMELiIdm_peFKdJiHbIwVtgu55X6z' -O serialized_dict.pkl
+
+# ./models/yolo/names.pkl
+# https://drive.google.com/file/d/1C4etbyFnMxfxaWvJ8HNqO_cwyrKxccX_/view?usp=sharing
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1C4etbyFnMxfxaWvJ8HNqO_cwyrKxccX_' -O ./models/yolo/names.pkl
+
+# ./models/yolo/colors.pkl
+# https://drive.google.com/file/d/1ImrRT-ERNYw3_fW-qVfl4JLQv55NvSY3/view?usp=sharing
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ImrRT-ERNYw3_fW-qVfl4JLQv55NvSY3' -O ./models/yolo/colors.pkl
+
+# ./models/yolo/yolov3-tiny.pt
+# https://drive.google.com/file/d/1k39G0CrSGvW23AuyJGIuVI1WKQSg1X3s/view?usp=sharing
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1k39G0CrSGvW23AuyJGIuVI1WKQSg1X3s' -O ./models/yolo/yolov3-tiny.pt
--- runner_yolo.sh
+++ runner_yolo.sh
@@ -5,8 +5,8 @@ source auto_gpu.sh
 echo "${CUDA_VISIBLE_DEVICES}"
 python main_yolo.py --bs=64 \
 --jitter=0 \
---path="/home/achawla/akshayws/ngc_results/yoloResults/test_new_repo/${now}" \
---iterations=8000 \
+--path="./yoloResults/test_new_repo/${now}" \
+--iterations=2000 \
 --r_feature=0.01 \
 --first_bn_coef=0.0 \
 --main_loss_multiplier=1.0 \
@@ -15,4 +15,4 @@ python main_yolo.py --bs=64 \
 --lr=0.005 \
 --wd=0.0 \
 --save_every=400 \
---display_every=1 # --cache_batch_stats --real_mixin_alpha=0.0
+--display_every=1 # --cache_batch_stats 
