3d4200af58da35c3dcae16ff129916cbb0b058e3
Akshay Chawla <achawla@nvidia.com>
2020-05-16 19:46:32 -0700

    Bugfixes in looped_runner_yolo.sh
    load images from 82k training images txt file
    export CUDA_VISIBLE_DEVICES so main_yolo.py can see it
    Use $OUTDIR when looking up verifier mAP values

--- looped_runner_yolo.sh
+++ looped_runner_yolo.sh
@@ -1,6 +1,7 @@
 SEED=$1 
 ITERS=$2 
-echo CUDA_VISIBLE_DEVICES=$3
+export CUDA_VISIBLE_DEVICES=$3
+echo $CUDA_VISIBLE_DEVICES
 echo "Starting with seed: ${SEED} for ${ITERS} iterations"
 
 TERMINALCONDITION=$( expr $SEED + $ITERS )
@@ -15,7 +16,7 @@ do
     python main_yolo.py --bs=32 \
     --jitter=30 --do_flip --mean_var_clip --rand_brightness --rand_contrast --random_erase \
     --path="/result/$OUTDIR" \
-    --train_txt_path="/dataset/coco/ngc_trainvalno5k.txt" \
+    --train_txt_path="/akshayws/ngc_train_82k.txt" \
     --iterations=4000 \
     --r_feature=0.002 \
     --first_bn_coef=10.0 \
@@ -29,8 +30,8 @@ do
     --seeds="0,0,${SEED}" --shuffle \
     --display_every=100 --init_scale=0.28661 --init_bias=0.48853 > /dev/null  
 
-    cat losses.log | grep "\[VERIFIER\] Real image mAP"
-    cat losses.log | grep "mAP VERIFIER" | tail -n1
+    cat /result/$OUTDIR/losses.log | grep "\[VERIFIER\] Real image mAP"
+    cat /result/$OUTDIR/losses.log | grep "mAP VERIFIER" | tail -n1
 
     SEED=$( expr $SEED + 1 )
 
