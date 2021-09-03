f39363cbd8d8abff5576b27ab40ec1251fc995a6
Akshay Chawla <chawla.akshay1234@gmail.com>
2021-09-03 00:18:48 -0700

    add license to top-level diode/image generation scripts

--- scripts/SEED_looped_runner_yolo.sh
+++ scripts/SEED_looped_runner_yolo.sh
@@ -1,3 +1,17 @@
+# --------------------------------------------------------
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+# Official PyTorch implementation of WACV2021 paper:
+# Data-Free Knowledge Distillation for Object Detection
+# A Chawla, H Yin, P Molchanov, J Alvarez
+# --------------------------------------------------------
+
+
 SEED=$1 
 ITERS=$2 
 export CUDA_VISIBLE_DEVICES=$3
